6a1c5cbad38dccbb06dff5210d75bc7616c97ee9
SDK <50303777+sdk-bens@users.noreply.github.com>
2023-06-02 09:54:50 -0700

    Update app.py
    added headers

--- app.py
+++ app.py
@@ -1,2 +1,4 @@
 import streamlit as st
 st.title("My CE App")
+st.header('Data Visualization Section')  # Sets a header for a section
+st.subheader('Subsection: Pie Chart Analysis')  # Sets a subheader for a subsection
