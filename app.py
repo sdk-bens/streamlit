a8cbd2940b5c5bb03aab547ca9a8c773a1f0849a
SDK <50303777+sdk-bens@users.noreply.github.com>
2023-06-02 10:01:35 -0700

    Update app.py
    added the data frame

--- app.py
+++ app.py
@@ -2,3 +2,12 @@ import streamlit as st
 st.title("My CE App")
 st.header('Data Visualization Section')  # Sets a header for a section
 st.subheader('Subsection: Pie Chart Analysis')  # Sets a subheader for a subsection
+import pandas as pd
+
+data = {
+    'A': [45, 37, 89, 67],
+    'B': ['John', 'Max', 'Lisa', 'Chris']
+}
+df = pd.DataFrame(data)
+
+st.write(df)
