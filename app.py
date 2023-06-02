0a36e84e562b2aa78a8e8e7ef26e72b5a8183f97
SDK <50303777+sdk-bens@users.noreply.github.com>
2023-06-02 10:02:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
+import pandas as pd
 st.title("My CE App")
 st.header('Data Visualization Section')  # Sets a header for a section
 st.subheader('Subsection: Pie Chart Analysis')  # Sets a subheader for a subsection
-import pandas as pd
 
+st.text('This section focuses on data preprocessing.')
+st.markdown('**This is some bold text**')
 data = {
     'A': [45, 37, 89, 67],
     'B': ['John', 'Max', 'Lisa', 'Chris']
